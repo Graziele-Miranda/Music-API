85acf7b680fbb5f8129d7fc5b0cc7793b78fb4d3
Graziele L. Miranda <lima.graziele@yahoo.com.br>
2023-06-08 12:02:19 -0300

    Estrutura inicial

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/albums", require("./control/AlbumAPI"));
+app.use("/artists", require("./control/ArtistAPI"));
+app.use("/songs", require("./control/MusicAPI"));
+app.use("/install", require("./control/InstallAPI"));
+
+app.listen(3000, () => {
+  console.log("Listenning...");
+});
